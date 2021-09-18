c13f8e2eb3a6e7b973979b28a1e0a8601db8eb64
Sang Nguyen <sangnguyendinh24work@gmail.com>
2021-09-18 11:06:24 +0700

    update firebase

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -8,13 +8,13 @@ import 'firebase/compat/storage'
 import 'firebase/compat/firestore'
 
 const firebaseConfig = {
-    apiKey: "AIzaSyA7VsKT-iQ3r94UXsuySc6n0hdChks3E8s",
-    authDomain: "social-web-app-a8bf4.firebaseapp.com",
-    projectId: "social-web-app-a8bf4",
-    storageBucket: "social-web-app-a8bf4.appspot.com",
-    messagingSenderId: "129508084505",
-    appId: "1:129508084505:web:2d655f34ecf3b8a5035388",
-    measurementId: "G-QET3Q2NLP8"
+    apiKey: FIREBASE_API_KEY,
+    authDomain: FIREBASE_AUTH_DOMAIN,
+    projectId: FIREBASE_PROJECT_ID,
+    storageBucket: FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: FIREBASE_MESSAGE_SENDER_ID,
+    appId: FIREBASE_APP_ID,
+    measurementId: FIREBASE_MEASUREMENT_ID,
 }
 
 // Initialize Firebase
